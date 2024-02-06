b13b97e1f49e667d52eb89698d3da68e2620c878
b.medoshin <jetun@ya.ru>
2024-02-06 15:23:27 +0300

    remove lodash

--- src/_dev/dev.interceptor.ts
+++ src/_dev/dev.interceptor.ts
@@ -1,38 +1,37 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
-import { cloneDeep } from 'lodash';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse } from '@angular/common/http'
+import { Observable, of } from 'rxjs'
 
 const userInfo = {
   name: 'Pete',
   age: 100,
-  licenses: [1230, 3213]
+  licenses: [1230, 3213],
 } as const
 
 const licenses = [
   {
     id: userInfo.licenses[0],
     name: 'can_dig',
-    description: 'This license confirms that the recipient of it can dig'
+    description: 'This license confirms that the recipient of it can dig',
   },
   {
     id: userInfo.licenses[1],
     name: 'can_rest',
-    description: 'This license confirms that the recipient of it can rest'
-  }
-] as const;
+    description: 'This license confirms that the recipient of it can rest',
+  },
+] as const
 
-export class DevInterceptor implements HttpInterceptor{
+export class DevInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const dupReq = req.clone({ url: '/proxy' + req.url });
-    return next.handle(dupReq);
+    const dupReq = req.clone({ url: '/proxy' + req.url })
+    return next.handle(dupReq)
 
     //stackblitz
-    if(req.method === 'GET' && req.url.includes('users')) {
-      return of(new HttpResponse({ status: 200, body: cloneDeep(userInfo) }));
-    } else if(req.method === 'GET' && req.url.includes('licenses')) {
-      return of(new HttpResponse({ status: 200, body: cloneDeep(licenses) }));
+    if (req.method === 'GET' && req.url.includes('users')) {
+      return of(new HttpResponse({ status: 200, body: structuredClone(userInfo) }))
+    } else if (req.method === 'GET' && req.url.includes('licenses')) {
+      return of(new HttpResponse({ status: 200, body: structuredClone(licenses) }))
     } else {
-      throw new Error();
+      throw new Error()
     }
   }
 }
